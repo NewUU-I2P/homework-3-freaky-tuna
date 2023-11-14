0b8f44cdb522405f6f6685a1bfe2e0ffcc749963
АДМИН <a.satmuratov@newuzbekistanuniversity.uz>
2023-11-14 23:46:28 +0500

    commit

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,19 +1,25 @@
 float problemSolution1(float consumed_water) {
     float cost;
-    if (consumed_water >= 0 and consumed_water <= 30){
-        cost = consumed_water*0.4;
+    if (consumed_water >= 30){
+        cost = 30*0.4;
         consumed_water -= 30;
-        if (consumed_water >=0 and consumed_water <= 20){
-            cost += consumed_water*0.12;
+        if (consumed_water >=20){
+            cost += 20*0.12;
             consumed_water -= 20;
             if (consumed_water >= 10){
-                cost += consumed_water*1.4;
+                cost += 10*1.4;
                 consumed_water -= 10;
                 if (consumed_water >= 0){
                     cost += consumed_water*1.5;
                 }
+            }else{
+                cost += consumed_water*1.4;
             }
+        }else{
+            cost += consumed_water *0.12;
         }
+    }else{
+        cost = consumed_water*0.4;
     }
     return cost;
 }
